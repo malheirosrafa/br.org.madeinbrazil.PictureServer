daecbcf7ea5a5d4b83330011280981324ddefd16
rafael malheiros <rafael_malheiros@hotmail.com>
2015-07-13 16:20:45 -0300

    adicionado metodo patch ao resource picture

--- resource/picture.js
+++ resource/picture.js
@@ -1,73 +1,128 @@
-'use strict';
+(function() {
+  'use strict';
 
-var fs = require('fs')
-  , gm = require('gm')
-  , config  = require('config')
-  , restify = require('restify')
-  , Picture = require('../model/picture')
-
-
-var picturesConfig = config.get('pictureTemplates')
+  var fs = require('fs'),
+    gm = require('gm'),
+    config = require('config'),
+    restify = require('restify'),
+    Picture = require('../model/picture');
 
 
-exports.get = function get(req, res, next) {
+  var picturesConfig = config.get('pictureTemplates');
 
-  var pictureFormat  = req.params.formatName
-    , pictureId = req.params.pictureId
 
-  var onPictureBufferReady = function onPictureBufferReady (error, buffer) {
-    if (error) return next(new restify.InternalServerError(error));
+  exports.get = function get(req, res, next) {
 
-    res.writeHead(200, {'Content-Type': 'image/jpg' });
-    res.end(buffer, 'binary');
-    return next();
-  }
+    var pictureFormat = req.params.formatName,
+      pictureId = req.params.pictureId;
 
-  var onPictureDocumentFound = function onPictureFound (error, picture) {
+    var onPictureBufferReady = function onPictureBufferReady(error, buffer) {
+      if (error) return next(new restify.InternalServerError(error));
 
-    if (error) return next(new restify.InternalServerError(error));
-    if (!picture) return next(new restify.NotFoundError());
+      res.writeHead(200, {
+        'Content-Type': 'image/jpg'
+      });
+      res.end(buffer, 'binary');
+      return next();
+    };
 
-    var cfg = picturesConfig[picture.template].formats[pictureFormat];
+    var onPictureDocumentFound = function onPictureFound(error, picture) {
 
-    if(!cfg) return next(new restify.BadRequestError('%s is not a valid format', pictureFormat));
+      if (error) return next(new restify.InternalServerError(error));
+      if (!picture) return next(new restify.NotFoundError());
 
-    var img = gm(picture.data, pictureId)
-          .resize(cfg.width, cfg.height, cfg.options);
+      var cfg = picturesConfig[picture.template].formats[pictureFormat];
 
-    if(cfg.crop)
-      img.gravity(cfg.gravity)
-          .crop(cfg.width, cfg.height);
+      if (!cfg) return next(new restify.BadRequestError('%s is not a valid format', pictureFormat));
 
-    img.quality(cfg.quality)
+      var img = gm(picture.data, pictureId)
+        .resize(cfg.width, cfg.height, cfg.options);
+
+      if (cfg.crop)
+        img
+        .gravity(cfg.gravity)
+        .crop(cfg.width, cfg.height);
+
+      img.quality(cfg.quality)
         .autoOrient()
         .noProfile()
         .toBuffer('JPG', onPictureBufferReady);
-  }
+    };
 
-  try{
-    Picture.findById(pictureId, onPictureDocumentFound);
-  } catch(e) {
+
+    try {
+      Picture.findById(pictureId, onPictureDocumentFound);
+    } catch (e) {
       return next(new restify.NotFoundError());
-  }
-}
+    }
+  };
 
 
-exports.create = function create(req, res, next) {
-  //console.log(req.files.picture);
+  exports.create = function create(req, res, next) {
+    //console.log(req.files.picture);
 
-  var type = req.files.picture.type;
+    var type = req.files.picture.type;
 
-  fs.readFile(req.files.picture.path, function (err, data) {
+    fs.readFile(req.files.picture.path, function(err, data) {
 
-    var pic = new Picture({template: req.params.template, data: data});
-    pic.save();
+      var pic = new Picture({
+        template: req.params.template,
+        data: data
+      });
+      pic.save();
 
-    var picCreated = {
-        id: pic._id
+      res.send();
+      next();
+    });
+  };
+
+
+  exports.update = function update(req, res, next) {
+
+    var pictureDataReady = false,
+      pictureDocumentReady = false,
+      pictureDocument = null,
+      pictureData = null;
+
+    var pictureId = req.params.pictureId;
+
+
+    var updatePictureData = function updatePictureData() {
+      if(!pictureFileReady || !pictureDataReady)
+        return;
+
+        pictureDocument.data = pictureData;
+        pictureDocument.save();
+    };
+
+    var onPictureFileReady = function onPictureFileReady(error, data) {
+
+      if (error) return next(new restify.InternalServerError(error));
+
+      pictureData = data;
+      pictureFileReady = true;
+      updatePictureData();
+    };
+
+    var onPictureDocumentFound = function onPictureDocumentFound(error, picture) {
+
+      if (error) return next(new restify.InternalServerError(error));
+      if (!picture) return next(new restify.NotFoundError());
+
+      pictureDocument = picture;
+      pictureDocumentReady = true;
+      updatePictureData();
+    };
+
+
+    try {
+      Picture.findById(pictureId, onPictureDocumentFound);
+      fs.readFile(req.files.picture.path, onPictureFileReady);
+    } catch (e) {
+      return next(new restify.NotFoundError());
     }
 
-    res.send(picCreated);
     next();
-  });
-}
+  };
+
+}());
--- serverWorker.js
+++ serverWorker.js
@@ -1,42 +1,48 @@
-'use strict';
+(function() {
+  'use strict';
 
-var config   = require('config')
-  , restify  = require('restify')
-  , mongoose = require("mongoose")
-  , packageJson = require('./package.json')
+  var config    = require('config'),
+    restify     = require('restify'),
+    mongoose    = require("mongoose"),
+    packageJson = require('./package.json');
 
-var pictureResource = require('./resource/picture.js');
+  var pictureResource = require('./resource/picture.js');
 
-var dbConfig = config.get('db');
-/*
- * Set up server
- * @return the created server
- */
-exports.spawn = function spawn (logger) {
+  var dbConfig = config.get('db');
+  /*
+   * Set up server
+   * @return the created server
+   */
+  exports.spawn = function spawn(logger) {
 
-  var server = restify.createServer({
-    name: packageJson.name,
-    version: packageJson.version
-  });
+    var server = restify.createServer({
+      name: packageJson.name,
+      version: packageJson.version
+    });
 
-  server.use(restify.bodyParser());
-  server.use(restify.gzipResponse());
-  server.use(restify.authorizationParser());
+    server.use(restify.bodyParser());
+    server.use(restify.gzipResponse());
+    server.use(restify.authorizationParser());
 
-  server.on('NotFound', function (req, res, next) {
-    if (logger) logger.debug('404', 'Request for ' + req.url + ' not found. No route.');
-    res.send(404, req.url + ' was not found');
-  });
+    server.on('NotFound', function(req, res, next) {
+      if (logger) logger.debug('404', 'Request for ' + req.url + ' not found. No route.');
+      res.send(404, req.url + ' was not found');
+    });
 
-  if (logger) server.on('after', restify.auditLogger({ log: logger }));
+    if (logger) server.on('after', restify.auditLogger({
+      log: logger
+    }));
 
-  server.post('/:template', pictureResource.create);
-  server.get('/:pictureId/:formatName', pictureResource.get);
+    server.get('/:pictureId/:formatName', pictureResource.get);
+    server.post('/:template', pictureResource.create);
+    server.patch('/:pictureId', pictureResource.update);
 
-  mongoose.connect(dbConfig.host, function (error) {
-    if(error)
-      console.error('erro ao conectar mongoDB');
-  });
+    mongoose.connect(dbConfig.host, function(error) {
+      if (error)
+        console.error('erro ao conectar mongoDB');
+    });
 
-  return server;
-}
+    return server;
+  };
+
+}());
